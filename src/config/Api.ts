785b0ff4821ddf8a9aaca2ee9c51e22b653cee7e
Abdul Khan <abdulkhan333c@gmail.com>
2025-11-24 20:25:08 +0530

    add

--- src/config/Api.ts
+++ src/config/Api.ts
@@ -1,6 +1,16 @@
 import axios from "axios"
 
-export const API_URL="http://localhost:5454/"
+// app.js or config.js
+
+export const API_URL = process.env.API_URL || "http://localhost:8081";
+
+console.log("API_URL:", API_URL);
+
+// export it if needed
+module.exports = { API_URL };
+
+
+// export const API_URL="http://localhost:5454/"
 
 export const api=axios.create({
     baseURL:API_URL,
